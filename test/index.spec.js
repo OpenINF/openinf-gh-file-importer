dc93b96cebc26d56eb80547ad08ea4751385d523
Restyled.io <commits@restyled.io>
2022-10-22 03:16:07 +0000

    style: apply changes from prettier restyler
    Signed-off-by: Restyled Commits <commits@restyled.io>

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,24 +6,28 @@
  * found in the LICENSE file at https://open.inf.is/license
  */
 
-const {mkdirSync, rmdirSync} = require('fs');
-const {GhFileImporter} = require('./index.js');
+const { mkdirSync, rmdirSync } = require("fs");
+const { GhFileImporter } = require("./index.js");
 
-const DIR_TEMP = './tmp';
+const DIR_TEMP = "./tmp";
 const URL_RAW_PROPOSALS_README =
-    'https://raw.githubusercontent.com/tc39/proposals/HEAD/README.md';
+  "https://raw.githubusercontent.com/tc39/proposals/HEAD/README.md";
 
-const ghFileImporter = new GhFileImporter({destDir : DIR_TEMP});
+const ghFileImporter = new GhFileImporter({ destDir: DIR_TEMP });
 
-rmdirSync(DIR_TEMP, {recursive : true});
+rmdirSync(DIR_TEMP, { recursive: true });
 mkdirSync(DIR_TEMP);
 
 (async () => {
-  let contents =
-      await ghFileImporter.importContentsFromUrl(URL_RAW_PROPOSALS_README);
-  console.log(contents + '\r\n\r\n');
+  let contents = await ghFileImporter.importContentsFromUrl(
+    URL_RAW_PROPOSALS_README
+  );
+  console.log(contents + "\r\n\r\n");
 
-  contents =
-      await ghFileImporter.importContents('tc39', 'proposals', 'README.md');
+  contents = await ghFileImporter.importContents(
+    "tc39",
+    "proposals",
+    "README.md"
+  );
   console.log(contents);
 })();
