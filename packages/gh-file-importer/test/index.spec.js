116d5a2e4dfbca08e9fe53303f512ba72d38dd5e
Derek Lewis <dereknongeneric@open.inf.is>
2023-05-11 06:56:41 -0400

    chore(test): rm copyright blurb comment
    Signed-off-by: Derek Lewis <dereknongeneric@open.inf.is>

--- packages/gh-file-importer/test/index.spec.js
+++ packages/gh-file-importer/test/index.spec.js
@@ -1,11 +1,3 @@
-/**
- * @license
- * Copyright OpenINF All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://open.inf.is/license
- */
-
 const { GhFileImporter } = require("../lib/index.js");
 const tempy = require("tempy");
 
